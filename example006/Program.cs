387f23e3d6d44b12347c4f7a68f06e0def12b435
Olga <o.o.kiseleva@yandex.ru>
2022-08-09 20:57:29 +0300

    пример 6

---
+++ example006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Машуля, красотуля!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine( username);
+
+}
